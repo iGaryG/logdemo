0f27fe7827fd99662ddb44531b89ec92153171d7
iGaryG <gaurav.gavas700@gmail.com>
2021-06-21 00:33:06 +0530

    Calling Add

--- logdemo/logdemo/Program.cs
+++ logdemo/logdemo/Program.cs
@@ -4,11 +4,11 @@ namespace LogDemo
 {
     public class Program
     {
-        /*static void Main(string[] args)
+        static void Main(string[] args)
         {
             var calculator = new Calculator.Calculator();
             Console.WriteLine($"5+3 = {calculator.Add(5, 3)}");
             /*Console.WriteLine($"The square root of 3.14159 is {Math.Sqrt(3.14159)}");*/
-        //}
+        }
     }
 }
