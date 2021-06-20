e1ee4b8da3dfb9477aea276dcf04e030589898ef
iGaryG <gaurav.gavas700@gmail.com>
2021-06-21 00:30:27 +0530

    Added Add method

--- logdemo/logdemo/Calculator/Calculator.cs
+++ logdemo/logdemo/Calculator/Calculator.cs
@@ -2,10 +2,9 @@
 {
     public class Calculator
     {
-        /*public int Add(int left, int right)
+        public int Add(int left, int right)
         {
             return left + right;
-        }*/
-        
+        } 
     }
 }
