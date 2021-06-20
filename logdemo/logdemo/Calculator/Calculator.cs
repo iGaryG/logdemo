12ebe7f81ecc7354fe21c32b954553ac0d5ccb4e
iGaryG <gaurav.gavas700@gmail.com>
2021-06-21 00:29:52 +0530

    Initial commit

---
+++ logdemo/logdemo/Calculator/Calculator.cs
@@ -0,0 +1,11 @@
+﻿namespace Calculator
+{
+    public class Calculator
+    {
+        /*public int Add(int left, int right)
+        {
+            return left + right;
+        }*/
+        
+    }
+}
---
+++ logdemo/logdemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LogDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculator = new Calculator.Calculator();
+            Console.WriteLine($"5+3 = {calculator.Add(5, 3)}");
+            /*Console.WriteLine($"The square root of 3.14159 is {Math.Sqrt(3.14159)}");*/
+        }
+    }
+}
