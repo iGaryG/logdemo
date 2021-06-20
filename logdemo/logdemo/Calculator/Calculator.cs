64784d0daca6896d667b68fd6a362f8a074c52d0
iGaryG <gaurav.gavas700@gmail.com>
2021-06-21 00:33:57 +0530

    Add Subtract to Calculator

--- logdemo/logdemo/Calculator/Calculator.cs
+++ logdemo/logdemo/Calculator/Calculator.cs
@@ -5,6 +5,10 @@
         public int Add(int left, int right)
         {
             return left + right;
-        } 
+        }
+        public int Subtract(int left, int right)
+        {
+            return left - right;
+        }
     }
 }
